a8d83279d995c90f17dbbbe7f389811d04c4d3e6
Ibrahim Erdem <erdem_ibo@yahoo.com>
2022-11-28 23:59:10 -0500

    added

---
+++ src/oop_principles/inheritance/FrontendTester.java
@@ -0,0 +1,7 @@
+package oop_principles.inheritance;
+
+public class FrontendTester extends Tester{
+    public FrontendTester(String fName, int age, String DOB, String SSN, boolean isAutomationTester) {
+        super(fName, age, DOB, SSN, isAutomationTester);
+    }
+}
